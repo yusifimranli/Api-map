0b26051b1aa8a9e827fdb0e59299a2e82326801c
yusifimranli <imranliyusif2006@gmail.com>
2026-03-12 13:17:27 +0400

    Update App.js

--- App.js
+++ App.js
@@ -8,24 +8,25 @@ function getData(){
                 <div class="bg-white border border-gray-200 shadow-md w-full max-w-sm rounded-lg overflow-hidden mt-4">
                 <div class="p-6">
                     <div>
-                        <h3 class="text-lg font-semibold">${item.name}</h3>
-                        <h3 class="text-lg font-semibold">${item.username}</h3>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.email}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.street}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.suite}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.city}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.zipcode}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.geo.lat}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.geo.lng}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.phone}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.website}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.name}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.catchPhrase}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.bs}</p>
+                        <h3 class="text-lg font-semibold">Name: ${item.name}</h3>
+                        <h3 class="text-lg font-semibold"><h3>Username: </h3>${item.username}</h3>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Email: </h1>${item.email}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Street: </h1> ${item.address.street}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Suite: </h1>${item.address.suite}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>City: </h1>${item.address.city}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Zipcode: </h1>${item.address.zipcode}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Lat: </h1>${item.address.geo.lat}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Lng: </h1>${item.address.geo.lng}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Phone: </h1>${item.phone}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Website: </h1>${item.website}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>Company-name: </h1>${item.company.name}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>catchPhrase: </h1>${item.company.catchPhrase}</p>
+                        <p class="mt-2 text-sm text-slate-500 leading-relaxed"><h1>company-bs: </h1>${item.company.bs}</p>
                         
                     </div>
                 </div>
     </div>`
             })
         })
+
 }
