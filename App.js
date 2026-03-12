9417a9b3040a550198fad9143d9221099a6cd768
yusifimranli <imranliyusif2006@gmail.com>
2026-03-12 13:29:21 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
-let user = document.getElementById('users')
-function getData(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => {
-            data.map(item =>{
-                user.innerHTML +=  `
-                <div class="bg-white border border-gray-200 shadow-md w-full max-w-sm rounded-lg overflow-hidden mt-4">
-                <div class="p-6">
-                    <div>
-                        <h3 class="text-lg font-semibold">${item.name}</h3>
-                        <h3 class="text-lg font-semibold">${item.username}</h3>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.email}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.street}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.suite}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.city}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.zipcode}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.geo.lat}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.address.geo.lng}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.phone}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.website}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.name}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.catchPhrase}</p>
-                        <p class="mt-2 text-sm text-slate-500 leading-relaxed">${item.company.bs}</p>
-                        
+    let user = document.getElementById('users')
+    function getData(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => {
+                data.map(item =>{
+                    user.innerHTML +=  `
+                    <div class="bg-white border border-gray-200 shadow-md w-full max-w-sm rounded-lg overflow-hidden mt-4">
+                    <div class="p-6">
+                        <div>
+                            <h3 class="text-lg font-semibold">Name: ${item.name}</h3>
+                            <h3 class="text-lg font-semibold">Username:${item.username}</h3>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Email: ${item.email}</>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Street: ${item.address.street}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Suite: ${item.address.suite}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">City: ${item.address.city}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Zipcode: ${item.address.zipcode}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Geo-Lat: ${item.address.geo.lat}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Geo-Lng: ${item.address.geo.lng}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Phone: ${item.phone}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Website: ${item.website}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Comp-Name: ${item.company.name}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Comp-catchPhrase: ${item.company.catchPhrase}</p>
+                            <p class="mt-2 text-sm text-slate-500 leading-relaxed font-semibold">Comp-Bs: ${item.company.bs}</p>
+                            
+                        </div>
                     </div>
-                </div>
-    </div>`
+        </div>`
+                })
             })
-        })
-}
+    }
